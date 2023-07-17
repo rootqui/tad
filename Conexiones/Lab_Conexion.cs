cb0a932430075fccfd275819ac7c05f737f276f3
Rodrigo Otazu <otazu.quispe.rodrigo@gmail.com>
2023-07-17 13:39:16 -0500

    codigo de conexion a base de datos

---
+++ Conexiones/Lab_Conexion.cs
@@ -0,0 +1,42 @@
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+// Creando una conexion por medio de la clase SqlConnectionStringBuilder
+SqlConnection objconexion = new SqlConnection();
+
+SqlConnectionStringBuilder objConstructorConexion = new SqlConnectionStringBuilder();
+objConstructorConexion.DataSource = ".";
+objConstructorConexion.InitialCatalog = "NombreBaseDeDatos";
+objConstructorConexion.IntegratedSecurity = true;
+objconexion.ConnectionString = objConstructorConexion.ConnectionString;
+
+// Creadondo una conexion por medio del archivo app.xonfig
+// Agregar referncias System.Configuration
+
+SqlConnection objCon = new SqlConnection();
+
+try
+{
+    //Recuperar la cadena de conexión del archivo app.config y utilizarla para configurar el objeto de conexión
+    //Creando lista de Conexiones
+    Dictionary<String, String> listaConexiones = CrearListaConexiones();
+    objCon.ConnectionString = listaConexiones["MiCadenaConexion"];
+    objCon.Open();
+    MessageBox.Show(objCon.State.ToString());
+}
+catch (Exception ex)
+{
+    MessageBox.Show(ex.Message);
+}finally
+{
+    objCon.Close();
+    MessageBox.Show(objCon.State.ToString());
+}
+
+

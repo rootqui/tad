c37663b7b46101e0cb4ffa62713e1422d05c140e
Rodrigo Otazu <otazu.quispe.rodrigo@gmail.com>
2023-07-17 15:40:45 -0500

    codigo de data adapter

---
+++ 04DataAdapter/FrmStoreProcedure.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Lab_DataAdapter
+{
+    public partial class FrmStoreProcedure : Form
+    {
+        SqlConnection objConexion = new SqlConnection();
+
+        // Instancia de la clase SqlDataAdapter
+        SqlDataAdapter objAdaptador = new SqlDataAdapter();
+
+        //Crear un objeto del DataSet
+        DataSet objDataSet = new DataSet();
+
+        public FrmStoreProcedure()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSincronizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                objAdaptador.Update(objDataSet.Tables["Prod"]);
+                MessageBox.Show("Los registros han sido sincronizados", "Notificación");
+            }
+            catch (SqlException sqlex)
+            {
+                MessageBox.Show(sqlex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                objConexion.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnectionStringBuilder objConstructorCadenaConexion = new SqlConnectionStringBuilder();
+                objConstructorCadenaConexion.DataSource = ".";
+                objConstructorCadenaConexion.InitialCatalog = "Lab_TAD";
+                objConstructorCadenaConexion.IntegratedSecurity = true;
+                objConexion.ConnectionString = objConstructorCadenaConexion.ConnectionString;
+
+                objAdaptador.SelectCommand = new SqlCommand("Seleccionar_Productos", objConexion);
+                objAdaptador.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                objAdaptador.InsertCommand = new SqlCommand("Insertar_producto",objConexion);
+                objAdaptador.InsertCommand.CommandType = CommandType.StoredProcedure;
+                //objAdaptador.InsertCommand.Parameters.AddWithValue("@PROD_NOMBRE", "PROD_NOMBRE");
+                objAdaptador.InsertCommand.Parameters.Add("@PROD_NOMBRE", SqlDbType.VarChar, 200, "PROD_NOMBRE");
+
+                objAdaptador.UpdateCommand = new SqlCommand("Actualizar_producto", objConexion);
+                objAdaptador.UpdateCommand.CommandType = CommandType.StoredProcedure;
+                objAdaptador.UpdateCommand.Parameters.Add("@PROD_ID", SqlDbType.Int, 0, "PROD_ID");
+                objAdaptador.UpdateCommand.Parameters.Add("@PROD_NOMBRE", SqlDbType.VarChar, 200, "PROD_NOMBRE");
+
+                objAdaptador.DeleteCommand = new SqlCommand("Eliminar_producto", objConexion);
+                objAdaptador.DeleteCommand.CommandType = CommandType.StoredProcedure;
+                objAdaptador.DeleteCommand.Parameters.Add("@PROD_ID", SqlDbType.Int, 0, "PROD_ID");
+
+                // El objDataSet, se le asigna el nombre "prod"  y se agrega al objAdaptador.
+                objAdaptador.Fill(objDataSet, "prod");
+
+                dataGridView1.DataSource = objDataSet.Tables["prod"];
+
+            }
+            catch (SqlException sqlex)
+            {
+                MessageBox.Show(sqlex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                objConexion.Close();
+            }
+        }
+    }
+}
